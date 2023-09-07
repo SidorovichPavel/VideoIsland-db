cc767057110e2c263508452d4850a264b0371247
SidorovichPavel <sidorovich.pasha@gmail.com>
2023-09-07 18:27:38 +0300

    remove trash

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 
-#include <soci/soci.h>
 #include <httpserver.hpp>
-
-#include <boost/chrono.hpp>
-
-#include <ranges>
+#include <soci/soci.h>
 
 int main(int argc, char* args[]){
+    
     std::cout << "Hello, from videoisland-db!\n";
 
 
