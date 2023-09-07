da2a31037c3c738f373805b453ea6f2e856a391a
SidorovichPavel <sidorovich.pasha@gmail.com>
2023-09-07 18:27:09 +0300

    init push

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <soci/soci.h>
+#include <httpserver.hpp>
+
+#include <boost/chrono.hpp>
+
+#include <ranges>
+
+int main(int argc, char* args[]){
+    std::cout << "Hello, from videoisland-db!\n";
+
+
+    return 0;
+}
